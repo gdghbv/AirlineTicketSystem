55f5584c9703e055adcefc747e07216b5ae49ad4
JustWorking <3546124151@qq.com>
2024-11-12 22:00:07 +0800

    加入了注册页面的ui

--- airline_vue/src/App.vue
+++ airline_vue/src/App.vue
@@ -1,15 +1,11 @@
+<script setup>
+  import login from './views/login.vue';
+</script>
+
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+ <login/>
 </template>
 
-<style>
-html,
-body,
-#app {
-  width: 100%;
-  height: 100%;
-}
-</style>
+<style scoped>
 
+</style>
--- airline_vue/vue.config.js
+++ airline_vue/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave:false,//将一些eslint的警告忽略掉，不进行eslint检测，项目完成后最后再开启eslint检测
+
 })
